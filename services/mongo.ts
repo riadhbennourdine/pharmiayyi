620dcea90d39edfc9a6ecb340838ca5ca4f2d9a8
Your Name <you@example.com>
2025-09-05 18:13:02 +0100

    feat: Add MongoDB connection

---
+++ services/mongo.ts
@@ -0,0 +1,26 @@
+import { MongoClient } from 'mongodb';
+
+const uri = process.env.MONGO_URL;
+
+if (!uri) {
+  throw new Error('Please define the MONGO_URL environment variable');
+}
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+if (process.env.NODE_ENV === 'development') {
+  // In development mode, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, {});
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  // In production mode, it's best to not use a global variable.
+  client = new MongoClient(uri, {});
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
